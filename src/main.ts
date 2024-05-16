a9505f0f8a089140309b7e2f7476ed9b410f4be7
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-16 20:30:05 +0200

    handle an edge case

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,40 @@ export async function clapToTmpVideoFilePath({
 
   outputDir = outputDir || (await getRandomDirectory())
 
-  const videoSegments = clap.segments.filter(s => s.category === ClapSegmentCategory.VIDEO && s.assetUrl.startsWith("data:video/"))
+  const alreadyAnEmbeddedFinalVideo = clap.segments.filter(s =>
+    s.category === ClapSegmentCategory.VIDEO &&
+    s.status === "completed" &&
+    s.startTimeInMs === 0 &&
+    s.endTimeInMs === clap.meta.durationInMs &&
+    s.assetUrl).at(0)
+
+  let ignoreThisVideoSegmentId = ""
+
+  if (alreadyAnEmbeddedFinalVideo) {
+    ignoreThisVideoSegmentId = alreadyAnEmbeddedFinalVideo?.id || ""
+
+    /*
+    you know what.. let's just ignore it, and re-generate fresh content
+    because most probably the user made an honest mistake
+
+    const outputFilePath = await writeBase64ToFile(
+      alreadyAnEmbeddedFinalVideo.assetUrl,
+      join(outputDir, `existing_final_video`)
+    )
+
+    return {
+      tmpWorkDir: outputDir,
+      outputFilePath
+    }
+    */
+  }
+
+  const videoSegments = clap.segments.filter(s =>
+    s.category === ClapSegmentCategory.VIDEO &&
+    s.assetUrl.startsWith("data:video/") &&
+    s.id !== ignoreThisVideoSegmentId
+  )
+  
   const storyboardSegments = clap.segments.filter(s => s.category === ClapSegmentCategory.STORYBOARD && s.assetUrl.startsWith("data:image/"))
 
   const canUseVideos = videoSegments.length > 0
